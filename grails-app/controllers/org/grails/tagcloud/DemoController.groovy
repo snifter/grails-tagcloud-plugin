292e0c3ae1575d5ff18096bf91d906d503cd28a1
Marek Podsiadły <marekpodsiadly@gmail.com>
2010-01-22 20:33:46 +0100

    DemoController added

---
+++ grails-app/controllers/org/grails/tagcloud/DemoController.groovy
@@ -0,0 +1,6 @@
+package org.grails.tagcloud
+
+class DemoController {
+
+    def index = { }
+}
---
+++ test/unit/org/grails/tagcloud/DemoControllerTests.groovy
@@ -0,0 +1,17 @@
+package org.grails.tagcloud
+
+import grails.test.*
+
+class DemoControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
