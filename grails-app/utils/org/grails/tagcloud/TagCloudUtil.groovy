b11e33b048f4b704fe45d4f42bd4316ca63d785f
Marek Podsiadły <marekpodsiadly@gmail.com>
2010-01-31 11:02:44 +0100

    comments modified and licence added

--- grails-app/utils/org/grails/tagcloud/TagCloudUtil.groovy
+++ grails-app/utils/org/grails/tagcloud/TagCloudUtil.groovy
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2010 Marek Podsiadły
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
 package org.grails.tagcloud
 
 /**
  * Utitlity class for tagcloud plugin.
  *
- * It create map of tags from class implements org.grails.taggable.Taggable
+ * It creates map of tags from class implements org.grails.taggable.Taggable
  * (from taggable plugin http://grails.org/plugin/taggable)
  *
  * @author Marek Podsiadły <marekpodsiadly@gmail.com>
